f1c121205ad3832ccf47a240c4b80e0089f89a51
harungurubudi <harunsadja@gmail.com>
2025-08-31 05:37:44 +0700

    feat(recipe-repository): add recipe delete repository

--- src/recipes/repository/recipe.repository.spec.ts
+++ src/recipes/repository/recipe.repository.spec.ts
@@ -18,7 +18,6 @@ describe('Repository - findOneBy', () => {
           provide: getRepositoryToken(RecipeEntity),
           useValue: {
             findOneBy: jest.fn(), // directly mock the method you need
-            save: jest.fn(),
           },
         },
       ],
@@ -29,7 +28,7 @@ describe('Repository - findOneBy', () => {
   });
 
   it('getByID - should return Recipe', async () => {
-    const id = 1
+    const id = 1;
     const now = new Date();
     const expected = new Recipe(RecipeID.of(id), "Chicken Curry", "45 min", "4 people", "onion, chicken, seasoning", 1000, now, now);
     const dbResult = RecipeEntity.fromDomain(expected);
@@ -42,15 +41,24 @@ describe('Repository - findOneBy', () => {
     expect(result).toEqual(ok(expected));
   });
 
-  it('getByID - should return error', async () => {
-    const id = 1
-    const expected = null;
+  it('getByID - should return error on not found', async () => {
+    const id = 1;
     (ormRepo.findOneBy as jest.Mock).mockResolvedValue(null);
 
     const result = await repo.getByID(RecipeID.of(id));
 
-    expect(ormRepo.findOneBy).toHaveBeenCalledWith({ id });
+    expect(ormRepo.findOneBy).toHaveBeenCalledWith({ id: id });
     expect(result).toEqual(error({ type: 'RecipeNotFoundError', error: new Error('No recipe found') }));
+  });
+
+  it('getByID - should thrown error', async () => {
+    const id = 1;
+    (ormRepo.findOneBy as jest.Mock).mockRejectedValue(new Error('any error'));
+
+    const result = await repo.getByID(RecipeID.of(id));
+
+    expect(ormRepo.findOneBy).toHaveBeenCalledWith({ id: id });
+    expect(result).toEqual(error({ type: 'RecipeNotFoundError', error: new Error('Failed to get recipe') }));
   });
 });
 
@@ -65,7 +73,6 @@ describe('Repository - create', () => {
         {
           provide: getRepositoryToken(RecipeEntity),
           useValue: {
-            findOneBy: jest.fn(), // directly mock the method you need
             save: jest.fn(),
           },
         },
@@ -79,11 +86,11 @@ describe('Repository - create', () => {
   it('create - should return newly created Recipe', async () => {
     const id = 1
     const payload = new RecipeInput("Chicken Curry", "45 min", "4 people", "onion, chicken, seasoning", 1000);
-    const dbInput  =  RecipeEntity.fromInput(payload);    
-    
+    const dbInput = RecipeEntity.fromInput(payload);
+
     const expected = new Recipe(RecipeID.of(id), "Chicken Curry", "45 min", "4 people", "onion, chicken, seasoning", 1000, new Date(), new Date());
     const entity = RecipeEntity.fromDomain(expected);
-    
+
     (ormRepo.save as jest.Mock).mockResolvedValue(entity);
 
     const result = await repo.create(payload);
@@ -94,13 +101,77 @@ describe('Repository - create', () => {
 
   it('create - should return error', async () => {
     const payload = new RecipeInput("Chicken Curry", "45 min", "4 people", "onion, chicken, seasoning", 1000);
-    const dbInput  =  RecipeEntity.fromInput(payload);    
-    
+    const dbInput = RecipeEntity.fromInput(payload);
+
     (ormRepo.save as jest.Mock).mockResolvedValue(null);
 
     const result = await repo.create(payload);
 
     expect(ormRepo.save).toHaveBeenCalledWith(dbInput);
-    expect(result).toEqual(error({ type: 'RecipeNotFoundError', error: new Error('Failed to create recipe') }));
+    expect(result).toEqual(error({ type: 'RecipeSaveError', error: new Error('Failed to create recipe') }));
+  });
+
+  it('create - should thrown error', async () => {
+    const payload = new RecipeInput("Chicken Curry", "45 min", "4 people", "onion, chicken, seasoning", 1000);
+    const dbInput = RecipeEntity.fromInput(payload);
+
+    (ormRepo.save as jest.Mock).mockRejectedValue(new Error('any error'));
+
+    const result = await repo.create(payload);
+
+    expect(ormRepo.save).toHaveBeenCalledWith(dbInput);
+    expect(result).toEqual(error({ type: 'RecipeSaveError', error: new Error('Failed to create recipe') }));
+  });
+});
+
+
+describe('Repository - delete', () => {
+  let repo: TypeOrmRecipeRepository;
+  let ormRepo: jest.Mocked<Repository<RecipeEntity>>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TypeOrmRecipeRepository,
+        {
+          provide: getRepositoryToken(RecipeEntity),
+          useValue: {
+            findOneBy: jest.fn(), // directly mock the method you need
+            delete: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    repo = module.get(TypeOrmRecipeRepository);
+    ormRepo = module.get(getRepositoryToken(RecipeEntity));
+  });
+
+  it('getByID - should return Ok', async () => {
+    const id = 1
+    const now = new Date();
+    const dbResult = RecipeEntity.fromDomain(new Recipe(RecipeID.of(id), "Chicken Curry", "45 min", "4 people", "onion, chicken, seasoning", 1000, now, now));
+
+    (ormRepo.findOneBy as jest.Mock).mockResolvedValue(dbResult);
+    (ormRepo.delete as jest.Mock).mockResolvedValue({ affected: 1 });
+
+    const result = await repo.delete(RecipeID.of(id));
+
+    expect(ormRepo.findOneBy).toHaveBeenCalledWith({ id });
+    expect(result).toEqual(ok(true));
+  });
+
+  it('getByID - should return Not found', async () => {
+    (ormRepo.findOneBy as jest.Mock).mockResolvedValue(null);
+    const result = await repo.delete(RecipeID.of(1));
+    expect(ormRepo.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    expect(result).toEqual(error({ type: 'RecipeDeleteError', error: new Error('No recipe found') }));
+  });
+
+  it('getByID - should thrown error', async () => {
+    (ormRepo.findOneBy as jest.Mock).mockRejectedValue(new Error('any error'));
+    const result = await repo.delete(RecipeID.of(1));
+    expect(ormRepo.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    expect(result).toEqual(error({ type: 'RecipeDeleteError', error: new Error('Failed to delete recipe') }));
   });
 });
--- src/recipes/repository/recipe.repository.ts
+++ src/recipes/repository/recipe.repository.ts
@@ -8,6 +8,7 @@ import { Result, ok, error } from "../../shared/result";
 export abstract class RecipeRepository {
   abstract getByID(id: RecipeID): Promise<Result<Recipe, RecipeError>>;
   abstract create(payload: RecipeInput): Promise<Result<Recipe, RecipeError>>
+  abstract delete(id: RecipeID): Promise<Result<boolean, RecipeError>>
 }
 
 @Injectable()
@@ -20,7 +21,7 @@ export class TypeOrmRecipeRepository implements RecipeRepository {
   constructor(
     @InjectRepository(RecipeEntity)
     private readonly repository: Repository<RecipeEntity>,
-  ) {}
+  ) { }
 
   /**
    * Retrieves a recipe by ID.
@@ -29,14 +30,15 @@ export class TypeOrmRecipeRepository implements RecipeRepository {
    * @returns the recipe with the given ID, or null if none exists
    */
   async getByID(id: RecipeID): Promise<Result<Recipe, RecipeError>> {
-    return this.repository
-      .findOneBy({ id: RecipeID.value(id) })
-      .then((recipe) => {
-        if (recipe) {
-          return ok(recipe.toDomain())
-        }
-        return error({ type: 'RecipeNotFoundError', error: new Error('No recipe found') })
-      })
+    try {
+      const recipe = await this.repository.findOneBy({ id: RecipeID.value(id) })
+      if (recipe) {
+        return ok(recipe.toDomain())
+      }
+      return error({ type: 'RecipeNotFoundError', error: new Error('No recipe found') })
+    } catch (e) {
+      return error({ type: 'RecipeNotFoundError', error: new Error('Failed to get recipe') })
+    }
   }
 
   /**
@@ -46,13 +48,30 @@ export class TypeOrmRecipeRepository implements RecipeRepository {
    * @returns the newly created recipe, or a RecipeError if something goes wrong
    */
   async create(payload: RecipeInput): Promise<Result<Recipe, RecipeError>> {
-    return this.repository
-      .save(RecipeEntity.fromInput(payload))
-      .then((recipe) => {
-        if (recipe) {
-          return ok(recipe.toDomain())
-        }
-        return error({ type: 'RecipeNotFoundError', error: new Error('Failed to create recipe') })
-      })
+    try {
+      const recipe = await this.repository.save(RecipeEntity.fromInput(payload))
+      if (recipe) {
+        return ok(recipe.toDomain())
+      }
+      return error({ type: 'RecipeSaveError', error: new Error('Failed to create recipe') })
+    } catch (e) {
+      return error({ type: 'RecipeSaveError', error: new Error('Failed to create recipe') })
+    }
   }
+
+  async delete(id: RecipeID): Promise<Result<boolean, RecipeError>> {
+    try {
+      const recipe = await this.repository.findOneBy({ id: RecipeID.value(id) });
+      if (!recipe) {
+        return error({ type: 'RecipeDeleteError', error: new Error('No recipe found') });
+      }
+
+      await this.repository.delete({ id: RecipeID.value(id) });
+      return ok(true);
+
+    } catch (e) {
+      return error({ type: 'RecipeDeleteError', error: new Error('Failed to delete recipe') });
+    }
+  }
+
 }
