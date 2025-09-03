074cd482977492f3b68a0369696394c7f261cdbe
harungurubudi <harunsadja@gmail.com>
2025-09-03 08:59:47 +0700

    fix: remove default app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { AdapterModule } from './adapter/adapter.module';
 
 @Module({
   imports: [RecipesModule, AdapterModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
